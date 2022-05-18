eab000c555335770f2db447514612ec07341a2f4
patriciadelacruzz <patriciaann.delacruz@letran.edu.ph>
2022-05-18 21:22:05 +0700

    firstcommit

---
+++ final_exam.session.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `final_examm`.`product` (
+  `idProduct` INT NOT NULL,
+  `ProductCode` VARCHAR(45) NULL,
+  `ProductName` VARCHAR(45) NULL,
+  `Description` VARCHAR(45) NULL,
+  `Price` VARCHAR(45) NULL,
+  PRIMARY KEY (`idProduct`));
